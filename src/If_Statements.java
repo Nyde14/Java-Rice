9786278cbc602ebdec0a326d037421118446770c
nyde14 <nydesioson13@gmail.com>
2024-10-28 06:56:33 +0800

    .

--- src/If_Statements.java
+++ src/If_Statements.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 public class If_Statements {
 
     public static void main(String[] args){
 
-        int age = 13;
+        System.out.println("How old are you?");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
         
         if(age>=75){
             System.out.println("Ok Boomer!");
@@ -15,8 +18,8 @@ public class If_Statements {
         else if (age>=13) {
             System.out.println("You are a teenager");
         }
-        else {
-            System.out.println("You are not an adult");
+        else if (age<13) {
+            System.out.println("You Want free candy?");
         }
 
 
