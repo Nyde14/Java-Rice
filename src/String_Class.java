e9e6ea886f5074069fba7559b89f946d6049ec3f
Nyde14 <nydesioson13@gmail.com>
2024-10-29 17:55:34 +0800

    Added String Class

---
+++ src/String_Class.java
@@ -0,0 +1,40 @@
+
+public class String_Class {
+
+    public static void main(String[] args){
+
+        String name = "name";
+
+        //boolean result = name.equals("name");
+        //int result = name.length();
+        //char result = name.charAt(0);
+        //int result = name.indexOf("n");
+        //boolean result = name.isEmpty();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.trim();
+        //String result = name.replace('a', 'e');
+
+        System.out.println(result);
+
+    }
+}
+// these are only a few ones that are mostly used
+/*
+name.equals compares 2 strings and outputs a boolean value which can be used for if statements
+to display its value
+
+name.length displays how long a string value is
+
+name.charAt displays a single letter of the string for example 0 = 1st letter
+
+name.indexOf finds and displays a letter and displays where can the letter be found
+
+name.isEmpty checks if a string value is empty
+
+name.toUpperCase makes all letters of the string value to upper case
+
+name.trim removes all spaces
+
+name.replace replaces a character with another one
+ */
